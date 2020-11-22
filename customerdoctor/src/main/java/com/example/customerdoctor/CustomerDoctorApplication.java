f4ca155240b5f9ffa13bda7f3aa365ba86bc481b
L-JAZZ <yelzhas.sultanov@gmail.com>
2020-11-22 19:35:18 +0600

    dashboard

--- customerdoctor/src/main/java/com/example/customerdoctor/CustomerDoctorApplication.java
+++ customerdoctor/src/main/java/com/example/customerdoctor/CustomerDoctorApplication.java
@@ -4,14 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableHystrixDashboard
 @EnableCircuitBreaker
 public class CustomerDoctorApplication {
-
 	@Bean
 	public RestTemplate getRestTemplate() {
 		return new RestTemplate();
