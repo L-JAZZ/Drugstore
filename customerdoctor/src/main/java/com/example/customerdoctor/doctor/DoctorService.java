ad1fe05c710d2134f4f3312ee0ceb10369c51037
L-JAZZ <yelzhas.sultanov@gmail.com>
2020-11-30 10:48:35 +0600

    security

--- customerdoctor/src/main/java/com/example/customerdoctor/doctor/DoctorService.java
+++ customerdoctor/src/main/java/com/example/customerdoctor/doctor/DoctorService.java
@@ -1,6 +1,7 @@
 package com.example.customerdoctor.doctor;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -11,7 +12,14 @@ public class DoctorService {
     @Autowired
     private RestTemplate restTemplate;
 
-    @HystrixCommand(fallbackMethod = "getFallbackDoctor")
+    @HystrixCommand(
+            fallbackMethod = "getFallbackDoctor",
+            threadPoolKey = "getDoctorPool",
+            threadPoolProperties = {
+                    @HystrixProperty(name = "coreSize",value = "20"),
+                    @HystrixProperty(name = "maxQueueSize",value = "10"),
+            }
+    )
     public Doctor getDoctor(Long id){
         return restTemplate.getForObject(
                 "http://localhost:8087/api/doctor/id/" + id,
